1d53905ccc360be2019fda60f10752ea4a7e8e66
Rahul Kumar <officialmail.kumar.rahul@gmail.com>
2026-04-12 21:44:40 +0530

    Update solution for sigmoid-numpy

--- sigmoid-numpy/sigmoid-numpy.py
+++ sigmoid-numpy/sigmoid-numpy.py
@@ -1,6 +1,6 @@
 import numpy as np
 
 def sigmoid(x):
-    x = np.asarray(x)
-    return 1 / (1 + np.exp(-x))
+    x = np.asarray(x) # this would basically convert the list to an ndarray in numpy
+    return 1 / (1 + np.exp(-x)) # bye-bye for loops, we don't care about how many nested loops are there, np will take care of them
     
